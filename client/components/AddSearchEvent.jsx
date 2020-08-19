5980fcdcce97700e28a70ecb1b064a858af629c2
Heather Friedman <hfried25@gmail.com>
2020-08-18 20:04:47 -0400

    can upload a photo, still working on functionality of adding to a post via prop drilling perhaps

--- client/components/AddSearchEvent.jsx
+++ client/components/AddSearchEvent.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import CreateEvent from './CreateEvent.jsx';
 import SearchEvent from './SearchEvent.jsx';
+import AddPhoto from './AddPhoto.jsx';
 
 export default function AddSearchEvent(props) {
   return (
     <div className="myCol">
-        <CreateEvent {...props}/>
-        <SearchEvent {...props}/>
+      <CreateEvent {...props} />
+      <AddPhoto {...props} />
+      <SearchEvent {...props} />
     </div>
-  )
+  );
 }
-
